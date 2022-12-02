a73ad85e3126b2cb2012ff2ad09f1f4e645559f3
Angela <116427072+anjela07@users.noreply.github.com>
2022-12-02 16:22:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const circles = document.querySelectorAll(".circle");
+console.log(circles);
+let activeLight = 0;
+
+setInterval(() =>{
+    changeLight();
+},2000);
+
+function changeLight(){
+    circles[activeLight].className = 'circle';
+    activeLight++;
+
+    if(activeLight > 2){
+        activeLight = 0;
+    }
+
+    const currentLight  = circles[activeLight];
+    currentLight.classList.add(currentLight.getAttribute
+        ("color"))
+}
